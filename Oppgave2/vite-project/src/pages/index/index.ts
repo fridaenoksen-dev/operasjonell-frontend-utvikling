028d031e49b74c6f35340d9457ca377482e852e5
DHawrami <dastan.hawrami@ruter.no>
2026-03-25 14:55:06 +0100

    la til TS

--- Oppgave2/vite-project/src/pages/index/index.ts
+++ Oppgave2/vite-project/src/pages/index/index.ts
@@ -1,11 +1,10 @@
-console.log("Hello from index.ts!");
-async function fetchData() {
+async function getUsers() {
   try {
     const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
-    console.log(data[0]);
+    console.log(data);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
-fetchData();
+getUsers();
