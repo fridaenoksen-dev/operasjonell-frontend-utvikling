0c52800b1c442be142c87313570cd25670edbca3
DHawrami <dastan.hawrami@ruter.no>
2026-03-29 19:45:11 +0200

    Ts

---
+++ src/Forside.ts
@@ -0,0 +1,19 @@
+async function getUsers() {
+    try {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error getUsers:", error);
+  }
+}
+getUsers();
+
+// lag en ny bruker//
+
+async function createUser() {
+  const response = await fetch("http://localhost:3000/api/users");
+  method: "POST",
+  Headers:
+
+}
